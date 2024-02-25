3fcf2429e3ccb3459756e35b0a5cd12c3c09e26a
Priynshu Sharma <priyanshusharma6666@gmail.com>
2024-02-25 11:01:33 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import applyRouter from "./routes/apply.route.js";
 import cookieParser from "cookie-parser";
-// import path from "path";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -29,12 +29,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/apply", applyRouter);
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
